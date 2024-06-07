86128c1bb50612497654972e5eddaba09fb98c1f
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-06-07 18:15:30 +0900

    　

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root "top#index"
   get "about" => "top#about", as: "about"
+
+  (1..18).each do |n|
+    get "lesson/step#{n}(/:name)" => "lesson#step#{n}"
+  end
 end
