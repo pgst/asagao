c5e6e034a2258451dd936daf305c65fee13b6207
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-06-11 16:42:18 +0900

    8.3完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_member
+
+  class LoginRequired < StandardError; end
+  class Forbidden < StandardError; end
+
   private
+
+  def login_required
+    raise LoginRequired unless current_member
+  end
 
   def current_member
     return Member.find(session[:member_id]) if session[:member_id]
   end
-
-  helper_method :current_member
 end
